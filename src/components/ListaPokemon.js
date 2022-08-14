3eb8e50c3a2cd034b2a299d8f01b1fa58e02900e
juvigh <juliolk.4321@gmail.com>
2022-08-14 20:55:33 -0300

    feat: add pagincao na lista

--- src/components/ListaPokemon.js
+++ src/components/ListaPokemon.js
@@ -6,34 +6,43 @@ function ListaPokemon() {
   const pokebola =
     "https://imagensemoldes.com.br/wp-content/uploads/2020/04/Pokebola-Pok%C3%A9mon-PNG-1024x1022.png";
   const [pokemons, setPokemons] = useState([]); 
-  const previousNext = '?offset=0&limit=10'
+  const [previousNext, setPreviousNext] = useState('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10')
 
-  useEffect(() => {
-     try {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${previousNext}`)
-      .then((response) => response.json())
-      .then((data) => setPokemons(data.results))
-     } catch (error) {
-       console.log('seu erro:', error)  
-     }
+  const pegarPokemons = async () => {
+    const res =  await fetch(previousNext)
+    const data = await res.json()
+    setPokemons(data.results)
+    setPreviousNext(data.next)
+  }
+
+  const previous = async () => {
+    const res = await fetch(previousNext)
+    const data = await res.json()  
+    setPokemons(data.results)
+    setPreviousNext(data.previous)
+  } 
+ 
+
+  useEffect(() =>{
+    pegarPokemons()
   }, [])
+
 
 
   return (
     <div className={styles.content}>
       <h3 className={styles.title}> Pokemons </h3>
       <ul className={styles.list}>
-        {pokemons.map((pokemon) => {
+        {pokemons.map((pokemon, index) => {
           return (
-            <li key={pokemon.id} className={styles.namePokemon}>
+            <li key={index} className={styles.namePokemon}>
               <img alt="pokebola" src={pokebola} /> {pokemon.name}
             </li>
-          );
-        })}
+          );})}
       </ul>
       <div className={styles.buttons}>
-        <button className={styles.button}> Voltar </button>
-        <button className={styles.button}> Avançar </button>
+        <button className={styles.button} onClick={() => previous()} > Voltar </button>
+        <button className={styles.button} onClick={() => pegarPokemons()} > Avançar </button>
       </div>
     </div>
   );
