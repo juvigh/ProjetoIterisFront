49fb3c5baad059c54059ac62fc1ced4d200584cd
juvigh <juliolk.4321@gmail.com>
2022-08-16 11:48:47 -0300

    feat: create easteregg

---
+++ src/components/Easteregg.js
@@ -0,0 +1,9 @@
+function Pikachu() {
+    return(
+        <div>
+            <img src='"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"'></img>
+        </div>
+    )
+}
+
+export default Pikachu
